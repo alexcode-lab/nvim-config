0039feb956fa9dc9dfcd82c403b58ad3ad20fecd
Alex Dorosh <dorosh.alexandr@gmail.com>
2025-11-04 20:28:05 +0200

    changes in tab width

--- lua/custom/config/settings.lua
+++ lua/custom/config/settings.lua
@@ -14,22 +14,18 @@ vim.cmd [[
   autocmd BufRead,BufNewFile *.blade.php set filetype=html
   autocmd FileType make setlocal noexpandtab shiftwidth=4 softtabstop=0 
   autocmd FileType nim setlocal noexpandtab shiftwidth=4 softtabstop=0
+  autocmd FileType php setlocal noexpandtab shiftwidth=4 softtabstop=0
 ]]
 
--- Set the number of spaces that a <Tab> character occupies
-vim.opt.tabstop = 4
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
--- Set the number of spaces to use for each step of (auto)indent
-vim.opt.shiftwidth = 4
+vim.o.autoindent = true
+vim.o.smarttab = true
+vim.o.fileformat = 'unix'
+vim.o.textwidth = 120
 
--- NOTE: Not sure if it needed
---
--- Set tab-specific settings for Makefiles
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'make',
---   callback = function()
---     vim.opt_local.expandtab = false -- Use tabs, not spaces
---     vim.opt_local.shiftwidth = 4 -- Set tab width to 8 spaces
---     vim.opt_local.softtabstop = 0 -- Disable space-to-tab conversion
---   end,
--- })
+vim.o.exrc = true
+vim.o.secure = true
