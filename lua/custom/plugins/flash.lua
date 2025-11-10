37e68747ce9aefd6d0a40c2a271fd6ed5de3fdd4
Alex Dorosh <dorosh.alexandr@gmail.com>
2025-11-10 23:21:31 +0200

    changed flash plugin settings

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
   opts = {},
   keys = {
     {
-      's',
+      'f',
       mode = { 'n', 'x', 'o' },
       function()
         require('flash').jump()
@@ -14,6 +14,11 @@ return {
   },
   config = function()
     require('flash').setup {
+      modes = {
+        search = { enabled = false },
+        char = { enabled = false },
+        treesitter = { enabled = false },
+      },
       highlight = {
         backdrop = false, -- disable graying out everything except matches
       },
