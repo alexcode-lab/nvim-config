8ccb24ccd2e98b6ffd7772534639f46f3f797cce
Alex Dorosh <dorosh.alexandr@gmail.com>
2026-09-06 14:10:14 +0300

    changes in keymaps

--- lua/plugins/text-case.lua
+++ lua/plugins/text-case.lua
@@ -1,13 +1,15 @@
 return {
   'johmsalas/text-case.nvim',
   config = function()
-    vim.keymap.set({ 'n', 'v' }, 'gas', ":lua require('textcase').current_word('to_snake_case')<CR>", { desc = 'Convert to [S]nake case' })
-    vim.keymap.set({ 'n', 'v' }, 'gac', ":lua require('textcase').current_word('to_camel_case')<CR>", { desc = 'Convert to [C]amel case' })
-    vim.keymap.set({ 'n', 'v' }, 'gap', ":lua require('textcase').current_word('to_pascal_case')<CR>", { desc = 'Convert to [P]ascal case' })
-    vim.keymap.set({ 'n', 'v' }, 'gat', ":lua require('textcase').current_word('to_title_case')<CR>", { desc = 'Convert to [T]title case' })
+    vim.keymap.set({ 'n', 'v' }, ',ts', ":lua require('textcase').current_word('to_snake_case')<CR>", { desc = 'Convert to [S]nake case' })
+    vim.keymap.set({ 'n', 'v' }, ',tc', ":lua require('textcase').current_word('to_camel_case')<CR>", { desc = 'Convert to [C]amel case' })
+    vim.keymap.set({ 'n', 'v' }, ',td', ":lua require('textcase').current_word('to_dot_case')<CR>", { desc = 'Convert to [D]ot case' })
+    vim.keymap.set({ 'n', 'v' }, ',tp', ":lua require('textcase').current_word('to_pascal_case')<CR>", { desc = 'Convert to [P]ascal case' })
+    vim.keymap.set({ 'n', 'v' }, ',tt', ":lua require('textcase').current_word('to_title_case')<CR>", { desc = 'Convert to [T]title case' })
+    vim.keymap.set({ 'n', 'v' }, ',tu', ":lua require('textcase').current_word('to_constant_case')<CR>", { desc = 'Convert to [U]pper case' })
   end,
   keys = {
-    'ga', -- Default invocation prefix
+    ',t', -- Default invocation prefix
   },
   cmd = {
     -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
